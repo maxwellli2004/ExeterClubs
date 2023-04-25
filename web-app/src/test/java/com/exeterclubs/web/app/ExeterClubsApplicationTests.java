e6782fbcac7c5e31bf96506079f4a294c8bd6202
Trevor Piltch <trevor@piltch.com>
2023-04-25 14:04:20 -0400

    Added web app

---
+++ web-app/src/test/java/com/exeterclubs/web/app/ExeterClubsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exeterclubs.web.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExeterClubsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
