9c5d3ba7481cb19108fd6fcf8ff4a3cba1c5e0e2
maxwellli2004 <126293510+maxwellli2004@users.noreply.github.com>
2023-05-01 14:19:30 -0400

    Create Event.java

---
+++ web-app/src/main/java/com/exeterclubs/web/app/Models/Event.java
@@ -0,0 +1,12 @@
+import java.util.UUID;
+import java.util.Date;
+import java.awt.Color;
+
+public class Event {
+	public String name;
+	public Date start;
+	public Date end;
+	public Date description;
+	//enum
+	public Color color;
+}
