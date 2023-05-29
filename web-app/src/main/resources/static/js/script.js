041df642866fb1677617febeccf84374007ae95c
maxwellli2004 <mhli2004@gmail.com>
2023-05-29 13:23:59 -0400

    some text

--- web-app/src/main/resources/static/js/script.js
+++ web-app/src/main/resources/static/js/script.js
@@ -73,3 +73,116 @@ function openDate(button) {
 function closeDate() {
   document.getElementById("dateForm").style.display = "none";
 }
+
+function outsideClickHandler(event) {
+  const clubForm = document.getElementById("dateForm");
+  // check if the clicked element is the popup form or a child of the form
+  if (!clubForm.contains(event.target)) {
+    closeDate();
+  }
+}
+
+var months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+var currentMonthIndex = 4; // may is at index 4
+var currentYear = 2023;
+
+function nextMonth() {
+  currentMonthIndex++; // increment the current month index
+
+  // check if the index exceeds the length of the months array
+  if (currentMonthIndex >= months.length) {
+    currentMonthIndex = 0; // reset back to the first month
+    currentYear++; // increment the year
+  }
+
+  // update the text content of the dynamicMonth element
+  var dynamicMonth = document.getElementById("dynamicMonth");
+  dynamicMonth.innerHTML =
+    months[currentMonthIndex] +
+    " " +
+    currentYear +
+    '<span style="font-size: 18px">';
+
+  // generate buttons for the new month
+  generateCalendarDates();
+}
+
+function prevMonth() {
+  currentMonthIndex--; // decrement the current month index
+
+  // check if the index goes below 0
+  if (currentMonthIndex < 0) {
+    currentMonthIndex = months.length - 1; // set it to the last month
+    currentYear--; // decrement the year
+  }
+
+  // update the text content of the dynamicMonth element
+  var dynamicMonth = document.getElementById("dynamicMonth");
+  dynamicMonth.innerHTML =
+    months[currentMonthIndex] +
+    " " +
+    currentYear +
+    '<span style="font-size: 18px">';
+
+  // generate buttons for the new month
+  generateCalendarDates();
+}
+
+function generateCalendarDates() {
+  var daysContainer = document.querySelector(".days");
+  daysContainer.innerHTML = "";
+
+  var totalDays = getDaysInMonth(currentYear, currentMonthIndex);
+  for (var i = 1; i <= totalDays; i++) {
+    var div = document.createElement("div");
+    div.classList.add("dateSquare");
+
+    var dateNumber = document.createElement("span");
+    dateNumber.textContent = i;
+    div.appendChild(dateNumber);
+
+    var eventsContainer = document.createElement("div");
+    eventsContainer.classList.add("dateEventContainer");
+
+    var event1 = createEventLink("Event 1", "./eventPage.html");
+    eventsContainer.appendChild(event1);
+
+    var event2 = createEventLink("Event 2", "./eventPage.html");
+    eventsContainer.appendChild(event2);
+
+    var event3 = createEventLink("Event 3", "./eventPage.html");
+    eventsContainer.appendChild(event3);
+
+    var event4 = createEventLink("Event 4", "./eventPage.html");
+    eventsContainer.appendChild(event4);
+
+    div.appendChild(eventsContainer);
+    daysContainer.appendChild(div);
+  }
+}
+
+function createEventLink(text, url) {
+  var eventLink = document.createElement("a");
+  eventLink.classList.add("dateEvent");
+  eventLink.href = url;
+  eventLink.textContent = text;
+  eventLink.appendChild(document.createElement("br")); // Add line break after each event link
+  return eventLink;
+}
+
+function getDaysInMonth(year, month) {
+  return new Date(year, month + 1, 0).getDate();
+}
